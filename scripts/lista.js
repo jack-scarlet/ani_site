8c1c7e3093b302d877e6deee20bc943a490901dc
jack-scarlet <82548176+jack-scarlet@users.noreply.github.com>
2024-10-13 14:11:52 -0300

    Update lista.js

--- scripts/lista.js
+++ scripts/lista.js
@@ -19,31 +19,18 @@ document.addEventListener('DOMContentLoaded', async function () {
         // Carrega os dados do JSON
         const response = await fetch('../list/anime_list.json');
         const data = await response.json();
-<!DOCTYPE html>
-<html lang="en">
 
         // Extrai as categorias (letras) dos dados
         const categories = Object.keys(data);
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
-    <link rel="stylesheet" type="text/css" href="style/estilo.css" media="screen" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="scripts/lista.js?v=20240315" defer></script>
 
         // Criação do menu
         const menu = document.getElementById('menu');
-</head>
 
         categories.forEach(category => {
             const button = document.createElement('button');
             button.textContent = category;
             button.classList.add('category-button');
             button.addEventListener('click', () => showAnimesByCategory(data[category]));
-<body>
 
             if (category === 'history') {
                 button.id = 'history-button';
@@ -51,17 +38,12 @@ document.addEventListener('DOMContentLoaded', async function () {
 
             menu.appendChild(button);
         });
-    <div id="botop">
-        <a href='/pages/manga_list_index'><button id="botaoMangas" class="botao-mangas botao">Mangas</button></a>
-        <a href='/pages/desenho_index'><button id="botaoDesenhos" class="botao-desenhos botao">Desenhos</button></a>
-    </div>
 
         // Remove o botão da categoria "history"
         const historyButton = document.getElementById('history-button');
         if (historyButton) {
             historyButton.remove();
         }
-    <div id="menu"></div>
 
         // Criação da caixa de pesquisa
         const searchContainer = document.getElementById('searchContainer');
@@ -69,43 +51,27 @@ document.addEventListener('DOMContentLoaded', async function () {
         searchInput.type = 'text';
         searchInput.placeholder = 'Pesquisar Anime';
         searchInput.id = 'searchInput';
-    <div id="searchContainer"></div>
 
         // Adiciona a caixa de pesquisa ao seu container
         searchContainer.appendChild(searchInput);
-    <button id="botaoDesenhos" class="botao-desenhos botao">Nuvem</button>
 
         // Criação do botão de pesquisa
         const searchButton = document.createElement('button');
         searchButton.id = 'searchButton';
-    <div id="animeGrid"></div>
 
         // Cria o ícone da lupa
         const searchIcon = document.createElement('i');
         searchIcon.classList.add('fas', 'fa-search'); // Adiciona as classes para o ícone da Font Awesome
         searchButton.appendChild(searchIcon); // Adiciona o ícone ao botão de pesquisa
-    <!-- Botão para apagar a URL do localStorage -->
-    <button id="apagarUrlButton">Apagar URL Armazenada</button>
 
         // Adiciona o botão de pesquisa ao seu container
         searchContainer.appendChild(searchButton);
-    <script>
-  
-        // Garante que o script só seja executado após o carregamento do DOM
-        document.addEventListener('DOMContentLoaded', function() {
-            // Função para apagar a URL do localStorage e recarregar a página
-            function apagarUrl() {
-                // Remove a URL armazenada no localStorage
-                localStorage.removeItem('url');
-                alert("URL removida do localStorage.");
 
         // Adiciona evento para pesquisa ao pressionar "Enter"
         searchInput.addEventListener('keypress', function (e) {
             if (e.key === 'Enter') {
                 searchByAnime(data);
-                // Recarrega a página
-                location.reload();
-}
+            }
         });
 
         // Adiciona evento de clique ao botão de pesquisa
@@ -150,12 +116,7 @@ function showAnimesByCategory(animes) {
             animeCard.appendChild(imageLink);
 
             animeGrid.appendChild(animeCard);
-            // Associa a função ao botão (verifica se o botão com ID 'apagarUrlButton' existe)
-            const apagarUrlButton = document.getElementById('apagarUrlButton');
-            if (apagarUrlButton) {
-                apagarUrlButton.addEventListener('click', apagarUrl);
-            }
-});
+        });
     } else {
         const noAnimeMessage = document.createElement('p');
         noAnimeMessage.textContent = 'Nenhum anime encontrado nestaa categoria.';
@@ -170,7 +131,6 @@ function searchByAnime(data) {
 
     // Filtra os animes que correspondem ao termo de pesquisa, excluindo a categoria 'history'
     const matchingAnimes = [];
-    </script>
 
     Object.keys(data).forEach(category => {
         if (category.toLowerCase() !== 'history') {
@@ -181,9 +141,7 @@ function searchByAnime(data) {
             });
         }
     });
-</body>
 
     // Exibe os animes correspondentes ou uma mensagem se nenhum for encontrado
     showAnimesByCategory(matchingAnimes);
 }
-</html>
